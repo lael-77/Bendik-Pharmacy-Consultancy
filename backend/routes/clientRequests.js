163c2811ac89b393aede503f1fa29349109b99cd
lael-77 <intwalilael@gmail.com>
2025-07-16 14:25:40 +0200

    new file

---
+++ backend/routes/clientRequests.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const clientRequestController = require('../controllers/clientRequestController');
+const authenticateJWT = require('../middlewares/auth');
+
+// Public: Submit a new client request
+router.post('/', clientRequestController.create);
+
+// Admin: Get all client requests (protected)
+router.get('/', authenticateJWT, clientRequestController.getAll);
+
+module.exports = router; 
