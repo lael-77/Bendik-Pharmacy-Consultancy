874fd02b092943bb60f935d5d33943cdea4ea5ad
lael-77 <intwalilael@gmail.com>
2025-07-26 18:24:07 +0300

    new

--- backend/routes/jobApplications.js
+++ backend/routes/jobApplications.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const jobApplicationController = require('../controllers/jobApplicationController');
 const { authenticateJWT } = require('../middlewares/auth');
 
-// Public: Submit a new job application with file upload
-router.post('/', jobApplicationController.createWithFile);
+// Public: Submit a new job application
+router.post('/', jobApplicationController.create);
 
 // Admin: Get all job applications (protected)
 router.get('/', authenticateJWT, jobApplicationController.getAll);
